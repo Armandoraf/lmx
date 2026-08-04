a9bd9ba3c7e4d97a51b5cdcfca2e45d9fdcfe4f0
Armandoraf <armandorafael2057@gmail.com>
2026-08-03 20:03:57 -0700

    Correct published LMX version metadata

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -32,7 +32,7 @@ type Native = {
 
 const core = native as Native;
 
-const packageVersion = '0.1.12';
+const packageVersion = '0.1.14';
 
 if (core.version() !== packageVersion) {
   throw new Error(
